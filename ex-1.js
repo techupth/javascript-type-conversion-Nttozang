dace7a69a94fc57b4f9b5e2e88dfbd0e4ac174ff
nut <suppanat.nut@gmail.com>
2024-03-28 20:42:21 +0700

    fix : string to number

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,10 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
+//console.log(typeof depositAmountFromUser);
+
+depositAmountFromUser = Number(depositAmountFromUser);
+
 accountBalance = accountBalance + depositAmountFromUser;
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
